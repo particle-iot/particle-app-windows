0f6ad059de1f4acc4cb6b3669eda6d33de74346d
Justin Myers <justmobilize@users.noreply.github.com>
2016-05-09 11:21:26 -0700

    Update name and version for desktop app

--- Particle.Tinker.Windows/Properties/AssemblyInfo.cs
+++ Particle.Tinker.Windows/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Particle")]
+[assembly: AssemblyProduct("Particle.Tinker.Windows")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("0.0.5.0")]
+[assembly: AssemblyFileVersion("0.0.5.0")]
 [assembly: ComVisible(false)]
